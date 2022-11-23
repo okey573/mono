de983cdce7de271c340301efb3e79bb97f79ab2b
ruifeng.kuang <ruifeng.kuang@vipshop.com>
2022-11-23 18:21:22 +0800

    fix(utils): 修改clone的类型推断

--- awesome-fe-utils/src/clone.ts
+++ awesome-fe-utils/src/clone.ts
@@ -31,14 +31,15 @@ const cloneString: ReturnSelfTypeFunction<string> = function (o) {
 // https://github.com/lodash/lodash/blob/master/.internal/baseClone.js
 
 const clone = function <T>(o: T): T {
-  // TODO 未完成
   const type = typeOf(o)
   switch (type) {
     case 'Boolean':
-      return cloneBoolean(o as boolean) as T
+      return cloneBoolean(o as unknown as boolean) as unknown as T
+    // TODO 其他类型未实现
     default:
-      return o
+      return o as unknown as T
   }
 }
 
 export default clone
+
