b11e51aacac5a18c3970f85b2846efce8ef132f7
ruifeng.kuang <ruifeng.kuang@vipshop.com>
2022-11-23 10:37:45 +0800

    fix(utils): 移除未完成方法

--- awesome-fe-utils/index.ts
+++ awesome-fe-utils/index.ts
@@ -2,7 +2,7 @@
 import packageInfo from './package.json'
 import delay from './src/delay'
 import typeOf from './src/typeOf'
-import clone from './src/clone'
+// import clone from './src/clone'
 
 const version: string = packageInfo.version
 
@@ -10,5 +10,5 @@ export {
   version,
   delay,
   typeOf,
-  clone
+  // clone
 }
