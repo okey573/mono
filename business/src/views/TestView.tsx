1b4c65d37c8e5a7f76027863967b79c8ecbdbb9f
ruifeng.kuang <ruifeng.kuang@vipshop.com>
2022-11-24 09:36:58 +0800

    feat(business): 添加element-plus

--- business/src/views/TestView.tsx
+++ business/src/views/TestView.tsx
@@ -1,15 +1,30 @@
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, watch } from 'vue'
 import { delay, version } from '@okey573/awesome-fe-utils'
 
 export default defineComponent({
   setup() {
+
+    const obj = reactive({
+      a1: {
+        b1: 'haha'
+      }
+    })
+
+    watch(() => obj.a1, (value, oldValue) => {
+      console.log('watched')
+      console.log(value === oldValue)
+      console.log(value)
+      console.log(oldValue)
+    }, { deep: true })
+
     const testFn = async () => {
       await delay(300)
       console.log(`utils version => ${ version }`)
+      obj.a1 = { b1: 'test' }
     }
 
     return () => <>
-      <button onClick={ testFn }>test button</button>
+      <el-button onClick={ testFn }>I am ElButton</el-button>
     </>
   }
 })
